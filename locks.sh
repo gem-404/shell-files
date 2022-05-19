572cb3f705c5b9d412528305b77b4c54a95d7faa
GEM-404 <mashaephantus2000@gmail.com>
2022-05-20 00:01:51 +0300

    Added my own locking configuration

---
+++ locks.sh
@@ -0,0 +1,19 @@
+#! /bin/zsh
+
+color=00000000
+pointer=default
+wrong="Fuck You"
+noinput="Come'ON"
+locktext="Huh, It's Locked Now!!!"
+wrongcolor="FF1A2E"
+
+# -e / --ignore-empty-password -> for ignoring empty passwords...
+# -f for showing passwords failed attempts
+# --wrong-color= # red color
+# --verif-color= # green color
+# --wrong-text="Fuck You"
+# --noinput-text="Enough Deletion Now"
+# --lock-text="Huh, It's Locked Now!!!"
+
+i3lock -ef --color=$color -p $pointer --wrong-text=$wrong --noinput-text=$noinput --lock-text=$locktext --wrong-color=$wrongcolor
+
